981ad0a1b21bf6e32e91aa657ddabfc65bcfc89d
Mark Gil Gutierrez <mark.gutierrez@firstperformance.com>
2024-03-19 10:32:28 +0800

    Initial Commit
    - added index.html
    - added resources
    - added script/index

---
+++ script/index.js
@@ -0,0 +1,52 @@
+let animationPlayed = false;
+
+window.addEventListener('scroll', function() {
+    let num = this.window.scrollY / this.window.innerHeight;
+
+    this.document.getElementById('ctr-text').innerHTML = num;
+  
+    // introduction
+
+    //for fading text
+    let intro_paragraphs =  this.document.querySelectorAll('.introduction .flex-box .text-img-box p');
+    intro_paragraphs.forEach((item) => item.style.opacity = num * .95)
+
+     //for fading images
+    let img_items = this.document.querySelectorAll('.introduction .flex-box .text-img-box .img img');
+
+    
+    if(num >= 1)
+    {
+        if(!animationPlayed)
+        {
+            img_items.forEach((item, index) => {
+                if(index % 2 == 0)
+                {
+                    item.style.animation =  "fade 1s, slideToLeft 1s";
+                }else
+                {
+                    item.style.animation =  "fade 1s, slideToRight 1s";
+                    
+                }
+                item.style.animationFillMode =  "forwards";
+               })
+               animationPlayed = true;
+        }
+       
+    }else
+    {
+        if(animationPlayed)
+        {
+            img_items.forEach((item, index) => {
+                item.style.opacity =  num;
+                item.style.animationFillMode =  "none";
+            })
+        }
+    }
+
+    this.document.getElementById("intro-title").style.scale = num;
+
+
+
+    // work image
+})
