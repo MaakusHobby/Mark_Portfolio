cc205fc1926d424fb1337134cf9bb3c4f4e2c3bc
Mark Gil Gutierrez <mark.gutierrez@firstperformance.com>
2024-03-20 15:16:52 +0800

    Added fonts
    - added work dates
    - adjusted animation time

--- script/index.js
+++ script/index.js
@@ -1,9 +1,60 @@
 let animationPlayed = false;
 
+var DateDiff = {
+ 
+    inDays: function(d1, d2) {
+        var t2 = d2.getTime();
+        var t1 = d1.getTime();
+ 
+        return Math.floor((t2-t1)/(24*3600*1000));
+    },
+ 
+    inWeeks: function(d1, d2) {
+        var t2 = d2.getTime();
+        var t1 = d1.getTime();
+ 
+        return parseInt((t2-t1)/(24*3600*1000*7));
+    },
+ 
+    inMonths: function(d1, d2) {
+        var d1Y = d1.getFullYear();
+        var d2Y = d2.getFullYear();
+        var d1M = d1.getMonth();
+        var d2M = d2.getMonth();
+ 
+        return (d2M+12*d2Y)-(d1M+12*d1Y);
+    },
+ 
+    inYears: function(d1, d2) {
+        return d2.getFullYear()-d1.getFullYear();
+    }
+}
+
+window.addEventListener("DOMContentLoaded", function() {   
+    let work_dates = this.document.querySelectorAll(".work-date")
+
+    console.log(work_dates)
+
+    work_dates.forEach((item, index) => {
+       temp_date =  item.innerHTML.split("-")
+
+        let date1 = new Date(temp_date[0]);
+        let date2 = new Date(temp_date[1]);
+
+        let yr =  DateDiff.inYears(date1, date2);
+
+        let yr_str = yr > 1 ? "years" : "year"; 
+
+        this.document.querySelectorAll(".work-date-diff")[index].innerHTML = yr + " " + yr_str 
+
+    })
+}); 
+
+
 window.addEventListener('scroll', function() {
     let num = this.window.scrollY / this.window.innerHeight;
 
-    this.document.getElementById('ctr-text').innerHTML = num;
+    // this.document.getElementById('ctr-text').innerHTML = num;
   
     // introduction
 
@@ -15,7 +66,7 @@ window.addEventListener('scroll', function() {
     let img_items = this.document.querySelectorAll('.introduction .flex-box .text-img-box .img img');
 
     
-    if(num >= 0.9)
+    if(num >= 0.75)
     {
         if(!animationPlayed)
         {
@@ -48,7 +99,15 @@ window.addEventListener('scroll', function() {
     this.document.getElementById("intro-text").style.scale = num + 1;
     this.document.getElementById("contact").style.scale = num * .5;
 
+    // Work Date
+    // const work_date = this.document.getElementById("work-date").innerHTML.split("-")
+    // let date1 = new Date(work_date[0]);
+    // let date2 = new Date(work_date[1]);
 
+    // let yr =  DateDiff.inYears(date1, date2);
+
+    // this.document.getElementById("work-date-diff").innerHTML = yr + "year/s"
+    
 
     // work image
 })
